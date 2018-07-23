47639029a438ccca86019cbd14bf8f8de75dc96d
Bastien Abadie <bastien@nextcairn.com>
2018-07-23 17:08:27 +0200

    Add footer with useful links, fixes #11

--- src/App.vue
+++ src/App.vue
@@ -25,40 +25,49 @@ export default {
 </script>
 
 <template>
-  <main>
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-      <div class="container is-fluid">
-        <div class="navbar-brand">
-          <div class="navbar-item">Static analysis</div>
-        </div>
-        <div class="navbar-menu">
+  <div id="app">
+    <main>
+      <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="container is-fluid">
+          <div class="navbar-brand">
+            <div class="navbar-item">Static analysis</div>
+          </div>
+          <div class="navbar-menu">
 
-          <div class="navbar-start">
-            <div class="navbar-item has-dropdown is-hoverable">
-              <span class="navbar-link">{{ channel }}</span>
-              <div class="navbar-dropdown is-boxed">
-                <a class="dropdown-item" v-for="c in channels" :class="{'is-active': c == channel}" v-on:click="switch_channel(c)">
-                  {{ c }}
-                </a>
+            <div class="navbar-start">
+              <div class="navbar-item has-dropdown is-hoverable">
+                <span class="navbar-link">{{ channel }}</span>
+                <div class="navbar-dropdown is-boxed">
+                  <a class="dropdown-item" v-for="c in channels" :class="{'is-active': c == channel}" v-on:click="switch_channel(c)">
+                    {{ c }}
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item" v-if="$route.name != 'stats'">
+                <router-link to="/stats" class="button is-link">All checks</router-link>
+              </div>
+              <div class="navbar-item" v-if="$route.name != 'tasks'">
+                <router-link to="/" class="button is-link">All tasks</router-link>
               </div>
             </div>
           </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item" v-if="$route.name != 'stats'">
-              <router-link to="/stats" class="button is-link">All checks</router-link>
-            </div>
-            <div class="navbar-item" v-if="$route.name != 'tasks'">
-              <router-link to="/" class="button is-link">All tasks</router-link>
-            </div>
-          </div>
         </div>
+      </nav>
+      <div class="container is-fluid">
+        <router-view></router-view>
       </div>
-    </nav>
-    <div class="container is-fluid">
-      <router-view></router-view>
-    </div>
-  </main>
+    </main>
+    <footer>
+      Built by <a href="https://wiki.mozilla.org/Release_Management" target="_blank">Release Management team</a>
+      <span>&bull;</span>
+      <a href="https://github.com/La0/mozilla-static-analysis" target="_blank">Source Code</a>
+      <span>&bull;</span>
+      <a href="https://github.com/La0/mozilla-static-analysis/issues" target="_blank">Report an issue</a>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
@@ -70,5 +79,34 @@ export default {
 
 div.navbar-item.has-dropdown {
   text-transform: capitalize;
+}
+
+/* Bottom footer support, it's not native in Bulma :( */
+div#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+div#app main {
+  flex: 1;
+}
+
+div#app footer {
+  border-top: 1px solid #CCC;
+  padding: 2px;
+  font-size: 0.9em;
+  color: #444;
+  background: #EEE;
+  text-align: right;
+}
+
+div#app footer a:hover {
+  text-decoration: underline;
+  color: #3273dc;
+}
+
+div#app footer span {
+  color: #CCC;
 }
 </style>
